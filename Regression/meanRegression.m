dc4e5f12875788d2f4c26b403cbc085b431334f4
JoeMzhao <mengjie.zhao@outlook.com>
2015-10-22 17:41:37 +0200

    mean regression - the most naive one

--- Regression/meanRegression.m
+++ Regression/meanRegression.m
@@ -1,9 +1,8 @@
 % ---------------------- meanRegression ---------------------------
 % The most naive regression method, one parameter model beta0
-% Function: beta = meanRegression(y,tX)
+% Function: beta = meanRegression(y)
 % Purpose : calculate the regression parameter beta 
 % Input   : y 	   --- the target vector of the given data set
-%			tX     --- the N-by-(D+1) matrix constructed from data
 % Output  : beta   --- parameter beta optimizing the cost
 % -------------- C. LIU & M. ZHAO ---------------------------------
 
